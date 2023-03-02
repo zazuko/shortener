cd74b64f538adfa40749d003ee5b9522acf4e5ea
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2023-03-02 16:06:50 +0100

    style: lint --fix

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 async function shorten(url, _fetch = fetch) {
-  const res = await _fetch("https://s.zazuko.com/api/v1/shorten/", {
-    method: "POST",
+  const res = await _fetch('https://s.zazuko.com/api/v1/shorten/', {
+    method: 'POST',
     body: new URLSearchParams({ url }),
-  });
+  })
 
   if (!res.ok) {
-    throw new Error(await res.text());
+    throw new Error(await res.text())
   }
 
-  return res.text();
+  return res.text()
 }
 
-module.exports = { shorten };
+module.exports = { shorten }
--- mocha-setup.js
+++ mocha-setup.js
@@ -1,5 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-const chai = require("chai");
-const sinonChai = require("sinon-chai");
+const chai = require('chai')
+const sinonChai = require('sinon-chai')
 
-chai.use(sinonChai);
+chai.use(sinonChai)
